daa1fddd19148a043a782885a5270cbd586a8211
Dustin Myers <dustint314@gmail.com>
2021-02-11 11:42:08 -0700

    wip

--- src/App.test.js
+++ src/App.test.js
@@ -9,7 +9,7 @@ test("Header is rendered", () => {
 
   // Act
   // query the DOM for the header element
-  const header = screen.getByText("Add New Animal");
+  const header = screen.getByText(/Add New Animal/i); // regex reg-ex regular expressions
 
   // Assert
   // Is the header found on the DOM?
