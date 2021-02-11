eb6a6fdb73ac256fe20b96c3e3a1b623363956c7
Dustin Myers <dustint314@gmail.com>
2021-02-11 10:46:07 -0700

    wip

--- src/App.test.js
+++ src/App.test.js
@@ -17,13 +17,13 @@
 //      - used for assertions (global functions)
 //      - uses global functions to structure our tests
 //           - test, describe, it
-test("test name", () => {
-  // if the test does not throw an error, the test will pass
-  // throw new Error("This test broke because of X, Y, Z"); // we will not throw errors in our tests... that is built into RTL
-});
+// test("test name", () => {
+//   // if the test does not throw an error, the test will pass
+//   // throw new Error("This test broke because of X, Y, Z"); // we will not throw errors in our tests... that is built into RTL
+// });
 
-describe("testing", () => {
-  it("should pass all the tests", () => {
-    console.log("it passed!");
-  });
-});
+// describe("testing", () => {
+//   it("should pass all the tests", () => {
+//     console.log("it passed!");
+//   });
+// });
